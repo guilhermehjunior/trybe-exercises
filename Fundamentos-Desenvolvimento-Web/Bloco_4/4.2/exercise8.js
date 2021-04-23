b56462f91c2c474601fb898a24abf6b68e7d2db6
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-04-23 16:16:56 -0300

    Adding exercises 7,8 and 9

---
+++ Fundamentos-Desenvolvimento-Web/Bloco_4/4.2/exercise8.js
@@ -0,0 +1,7 @@
+//exercise8
+let newArray =[];
+for(let index =0;index<25;index+=1){
+    newArray.push(index+1);
+    console.log(newArray[index]);
+}
+
---
+++ Fundamentos-Desenvolvimento-Web/Bloco_4/4.2/exercise9.js
@@ -0,0 +1,6 @@
+//exercise9
+let newArray =[];
+for(let index =0;index<25;index+=1){
+    newArray.push(index+1);
+    console.log(newArray[index]/2);
+}
