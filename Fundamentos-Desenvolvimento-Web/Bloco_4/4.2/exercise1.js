fbce3d278079233070b45132643bb5dd72c49592
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-04-23 15:56:13 -0300

    Adding exercises 1 and 2

---
+++ Fundamentos-Desenvolvimento-Web/Bloco_4/4.2/exercise1.js
@@ -0,0 +1,10 @@
+//Exercise 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index<numbers.length;index+=1){
+//     console.log(numbers[index]);
+// }
+
+for(let number of numbers){
+    console.log(number);
+}
