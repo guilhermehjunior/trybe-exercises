b8d59a8bb0d27f7aae816702f4932a41abe545da
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-04-26 15:32:41 -0300

    adding the exercise 2

---
+++ Fundamentos-Desenvolvimento-Web/Bloco_4/4.3/exercise2.js
@@ -0,0 +1,10 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+n= 5;
+
+let asteriscos="";
+
+for (let row = 1; row<=n; row+=1){
+    asteriscos+= "*";
+    console.log(asteriscos);
+}
