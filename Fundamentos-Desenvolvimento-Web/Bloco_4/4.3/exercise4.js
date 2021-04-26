ebfd74f2c06837b69a8d37ff9e5ca2feea8da13b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-04-26 18:44:08 -0300

    Adding exercise 5 and fixing exercise 4 eternal loop

--- Fundamentos-Desenvolvimento-Web/Bloco_4/4.3/exercise4.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_4/4.3/exercise4.js
@@ -3,11 +3,11 @@
 let n = 7;
 let asteriscos="";
 
-for(let row = 1; row<=n;row+=1){
+for(let row=1; row<=n;row+=1){
     asteriscos += "*";
     let space = "";
     if (row%2!==0){
-        for(let spaceIndex = (n-row)/2;spaceIndex>0;spaceIndex=1){
+        for(let spaceIndex = (n-row)/2;spaceIndex>0;spaceIndex-=1){
             space += " ";
         };
         console.log(space+asteriscos+space);
