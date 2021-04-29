6acf8bd11932d21fe96a7a6429579c8ed5f65840
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-04-29 18:18:54 -0300

    adding exercise 1 from day 5.1

--- Fundamentos-Desenvolvimento-Web/Bloco_5/5.1/script.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_5/5.1/script.js
@@ -1,19 +1,18 @@
-let head = document.getElementById("header-container");
+const head = document.getElementById("header-container");
 head.style.backgroundColor = "lightgreen";
 
-let urgente = document.getElementsByClassName("emergency-tasks");
+const urgente = document.getElementsByClassName("emergency-tasks");
 urgente[0].style.backgroundColor = "lightpink";
-
-let naoUrgente = document.getElementsByClassName("no-emergency-tasks");
+const naoUrgente = document.getElementsByClassName("no-emergency-tasks");
 naoUrgente[0].style.backgroundColor = "lightyellow";
 
-let titulosUrgentes = document.querySelectorAll(".emergency-tasks h3");
+const titulosUrgentes = document.querySelectorAll(".emergency-tasks h3");
 titulosUrgentes[0].style.backgroundColor = "purple";
 titulosUrgentes[1].style.backgroundColor = "purple";
 
-let titulosNaoUrgentes = document.querySelectorAll(".no-emergency-tasks h3");
+const titulosNaoUrgentes = document.querySelectorAll(".no-emergency-tasks h3");
 titulosNaoUrgentes[0].style.backgroundColor = "black";
 titulosNaoUrgentes[1].style.backgroundColor = "black";
 
-let footer = document.getElementById("footer-container");
+const footer = document.getElementById("footer-container");
 footer.style.backgroundColor = "darkgreen";
