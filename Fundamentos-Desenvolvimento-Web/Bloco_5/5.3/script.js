6456c7ecde53b8f0f93e54050a8741f288e6b3f4
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-03 19:38:42 -0300

    adding exercise 8

--- Fundamentos-Desenvolvimento-Web/Bloco_5/5.3/script.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_5/5.3/script.js
@@ -120,4 +120,18 @@ for (let index = 0; index < dezDaysList.length; index += 1) {
     }
 
     addingTasks('cozinhar');
-    
+
+//     Exercício 8:
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+    function addingSubtitles(string) {
+        const newSubtitle = document.createElement('div');
+        newSubtitle.style.backgroundColor = string;
+        newSubtitle.classList.add('task');
+        myTasks.appendChild(newSubtitle);
+    }
+
+    addingSubtitles('red');
+
