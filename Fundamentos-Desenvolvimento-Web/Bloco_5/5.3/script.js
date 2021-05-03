167f68ef03c540dd00fdfdfdc96913cedd225ee5
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-03 20:09:13 -0300

    adding exercise 10

--- Fundamentos-Desenvolvimento-Web/Bloco_5/5.3/script.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_5/5.3/script.js
@@ -143,4 +143,19 @@ for (let index = 0; index < dezDaysList.length; index += 1) {
     tarefaSelecionada.addEventListener('click', function (event) {
         event.target.classList.toggle('task-selected');
         event.target.classList.toggle('task');
-    })
+    });
+
+// 10 - Implemente uma função que adiciona um evento que ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+// Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+    numeroDias[index].addEventListener('click', function (event){
+        const taskSelected = document.querySelector('.task-selected');
+        if (taskSelected !== null) {
+            event.target.style.color = taskSelected.style.backgroundColor;
+        } else {
+            event.target.style.color = 'rgb(119,119,119)';
+        }
+    });
+    }
