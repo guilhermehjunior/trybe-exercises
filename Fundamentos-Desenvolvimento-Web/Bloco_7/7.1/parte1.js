eb6abbe0453be5f0a5bdb5ccd19a92fbd34cc657
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-19 16:35:29 -0300

    finalizando ex2 da parte2

--- Fundamentos-Desenvolvimento-Web/Bloco_7/7.1/parte1.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_7/7.1/parte1.js
@@ -15,6 +15,20 @@ testingScope(true);
 // Exercicio 2
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+const ordenandoArray = (array) => {
+  let newArray = array;
+  let menorValor = 0;
+  for (let index1 = 0; index1 < newArray.length; index1 += 1) {
+    for (let index = 0; index < newArray.length; index += 1) {
+      if (newArray[index1] < newArray[index]) {
+        menorValor = newArray[index];
+        newArray[index] = newArray[index1];
+        newArray[index1] = menorValor;
+      }
+    }
+  }
+  return newArray;
+}
 
-console.log(oddsAndEvens);
+console.log(`Os numeros ${ordenandoArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
+console.log(`Os numeros ${oddsAndEvens.sort((a,b) => a-b)} se encontram ordenados de forma crescente!`);
