6e188208631c7ed25b55e825ed7e783423b4bfd0
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-20 15:58:10 -0300

    adicionando exercicios 1-3 da parte2

--- Fundamentos-Desenvolvimento-Web/Bloco_7/7.2/parte2.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_7/7.2/parte2.js
@@ -17,3 +17,25 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+// 1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const adicionarTurno = (objeto, key, valor) => {
+  objeto[key] = valor;
+};
+
+adicionarTurno(lesson2,'turno','manhã');
+
+console.log(lesson2);
+
+// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listarKeysObjeto = (objeto) => console.log(Object.keys(objeto));
+
+listarKeysObjeto(lesson1);
+
+// 3 -Crie uma função para mostrar o tamanho de um objeto.
+
+const tamanhoObjeto = (objeto) => console.log(Object.keys(objeto).length);
+
+tamanhoObjeto(lesson1);
