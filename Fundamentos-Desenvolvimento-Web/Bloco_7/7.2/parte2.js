310d0c35485ab7a16ce718da162a7f70da819b44
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-20 16:20:37 -0300

    adicionando exercicios 4-6 parte2

--- Fundamentos-Desenvolvimento-Web/Bloco_7/7.2/parte2.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_7/7.2/parte2.js
@@ -39,3 +39,41 @@ listarKeysObjeto(lesson1);
 const tamanhoObjeto = (objeto) => console.log(Object.keys(objeto).length);
 
 tamanhoObjeto(lesson1);
+
+// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listarValoresObjeto = (objeto) => console.log(Object.values(objeto));
+
+listarValoresObjeto(lesson1);
+
+// 5 -Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+
+const novaLesson1 = {
+  lesson1,
+  lesson2,
+  lesson3
+};
+
+const allLessons = Object.assign({},{lesson1,lesson2,lesson3});
+
+console.log('----------');
+console.log(allLessons);
+console.log('----------');
+console.log(novaLesson1);
+
+// 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const totalEstudantes = (objeto) => {
+  const lessons = Object.keys(objeto);
+  const novoObjeto = {};
+  for (let licao = 0; licao < lessons.length; licao += 1) {
+    // console.log(objeto[lessons[licao]].numeroEstudantes);
+    novoObjeto[`lessons${licao+1}`] = {};
+    novoObjeto[`lessons${licao+1}`].numeroEstudantes = objeto[lessons[licao]].numeroEstudantes;
+  }
+  return novoObjeto;
+};
+
+console.log(totalEstudantes(allLessons));
+
+
