0d8999dfba93e179f30b8d0f1ae1e974763f6bf8
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-21 21:17:35 -0300

    adicionando correcoes

--- Fundamentos-Desenvolvimento-Web/Bloco_7/7.3/bonus-ex5.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_7/7.3/bonus-ex5.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const verify = (password) => {
   if (password === null) return false
-  if (password.length < 8) return false;
+  if (password.length <= 8) return false;
   const regexNumber = /[0-9]/;
   const regexLowerCase = /[a-z]/;
   const regexUpperCase = /[A-Z]/;
@@ -15,7 +15,7 @@ const verify = (password) => {
 assert.strictEqual(typeof verify, 'function');
 assert.strictEqual(verify('23af45A'), false);
 assert.strictEqual(verify(null), false);
-assert.strictEqual(verify('asfsdSac'), false);
-assert.strictEqual(verify('7sfsd7ac'), false);
-assert.strictEqual(verify('2145123A'), false);
-assert.strictEqual(verify('a56scSV0'), true);
+assert.strictEqual(verify('asfsdSaca'), false);
+assert.strictEqual(verify('7sfsd7aca'), false);
+assert.strictEqual(verify('2145123AA'), false);
+assert.strictEqual(verify('a56scSV0a'), true);
