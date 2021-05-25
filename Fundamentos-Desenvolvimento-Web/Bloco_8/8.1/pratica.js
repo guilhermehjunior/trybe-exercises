07c66c48fcc4e9efd429c2520da0cfad96f5babc
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-25 15:27:56 -0300

    adicionando primeiro exercicio

---
+++ Fundamentos-Desenvolvimento-Web/Bloco_8/8.1/pratica.js
@@ -0,0 +1,19 @@
+// Exercicio1
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiz Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const infos = (nome) => ({
+  nomeCompleto: nome,
+  email: `${nome.replace(' ', '_')}@trybe.com.br`,
+});
+
+console.log(newEmployees(infos));
+
+// Exercicio2
