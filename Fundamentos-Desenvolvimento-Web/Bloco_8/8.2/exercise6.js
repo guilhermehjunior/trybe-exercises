659c8a80aa0fa347016ec2dcba4f5979c5d6ee35
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-26 16:16:52 -0300

    adicionando ex6

--- Fundamentos-Desenvolvimento-Web/Bloco_8/8.2/exercise6.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_8/8.2/exercise6.js
@@ -64,3 +64,12 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  // escreva seu código aqui
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
+}
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
