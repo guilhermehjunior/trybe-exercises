3aab0ab0cb44f7a978cced0d28b4e2fc984c92a6
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-27 18:08:38 -0300

    adicionando ex5

--- Fundamentos-Desenvolvimento-Web/Bloco_8/8.3/exercise5.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_8/8.3/exercise5.js
@@ -64,3 +64,22 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  const livrosFantasiaFiccao = books.filter((book)=> 
+  book.genre === 'Fantasia' || 
+  book.genre === 'Ficção Científica');
+  return livrosFantasiaFiccao.map((book) => book.author.name)
+  .sort((a, b) => a > b);
+}
+
+console.log(fantasyOrScienceFictionAuthors());
+
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
