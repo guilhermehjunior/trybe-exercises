820a5fedc186a4920cd98bcdb2159dedc09449bc
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-27 16:10:06 -0300

    adicionando ex2

--- Fundamentos-Desenvolvimento-Web/Bloco_8/8.3/exercise2.js
+++ Fundamentos-Desenvolvimento-Web/Bloco_8/8.3/exercise2.js
@@ -64,3 +64,43 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  const idadeAutor = books.map((book) => 
+  ({
+    age: book.releaseYear - book.author.birthYear,
+    author: book.author.name,
+  }));
+  return idadeAutor.sort((a, b) => a.age - b.age);
+}
+
+console.log(nameAndAge());
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
