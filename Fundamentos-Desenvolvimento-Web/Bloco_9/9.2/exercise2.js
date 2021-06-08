604ef7c75bbd0786a1f4289d3c2c9ee1e7fd8660
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-08 19:19:53 -0300

    adicionando exercicios 1,2,3

---
+++ Fundamentos-Desenvolvimento-Web/Bloco_9/9.2/exercise2.js
@@ -0,0 +1,26 @@
+// const elevadoAoQuadrado = (num) => num * num;
+
+// const gerandoArrayAleatorio = (tamanho) => {
+//   const nwArray = [];
+//   for (let index = 0; index < tamanho; index += 1) {
+//     nwArray.push(elevadoAoQuadrado(Math.round(Math.random()*50)));
+//   }
+//   return nwArray;
+// }
+
+// const arrayAleatorio = gerandoArrayAleatorio(10);
+
+
+const arrayAleatorio = Array.from({length:10}, () => Math.floor(Math.random() * 50 + 1));
+
+const arrayAleatorioElevadoDois = arrayAleatorio.map((num)=> num * num);
+
+const funcaoPromise = new Promise((resolve, reject) => {
+  const soma = arrayAleatorioElevadoDois.reduce((acc, currentValue) => acc + currentValue);
+  if (soma < 8000) resolve(soma);
+  reject('Eh mais de oito mil!');
+});
+
+funcaoPromise
+  .then((response) => [2, 3, 4, 5].map((numero) => response / numero))
+  .catch((error) => console.log(error));
