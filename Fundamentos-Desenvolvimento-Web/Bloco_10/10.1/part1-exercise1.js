a286bf69974843d44f61362c2ab93dc6375df726
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-14 16:46:26 -0300

    remove pasta 10.1

--- Fundamentos-Desenvolvimento-Web/Bloco_10/10.1/part1-exercise1.js
+++
@@ -1,8 +0,0 @@
-const sum = (a, b) => {
-  if (typeof a !== 'number' || typeof b !== 'number'){
-    throw new Error('It is not a number');
-  }
-  return a + b;
-} 
-
-module.exports = sum;
--- Fundamentos-Desenvolvimento-Web/Bloco_10/10.1/part1-exercise2.js
+++
@@ -1,9 +0,0 @@
-const myRemove = (arr, item) => {
-  const nwArr = [];
-  for (let index = 0; index < arr.length; index += 1){
-    if (arr[index] !== item) nwArr.push(arr[index]);
-  }
-  return nwArr;
-}
-
-module.exports = myRemove;
