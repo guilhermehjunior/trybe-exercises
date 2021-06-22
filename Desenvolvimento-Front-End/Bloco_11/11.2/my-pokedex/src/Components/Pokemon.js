86a14904b8ac923fb319b6d8dfb8ed54690c9bc3
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-22 19:02:43 -0300

    adicionando propTypes ao componente Pokemon

--- Desenvolvimento-Front-End/Bloco_11/11.2/my-pokedex/src/Components/Pokemon.js
+++ Desenvolvimento-Front-End/Bloco_11/11.2/my-pokedex/src/Components/Pokemon.js
@@ -21,7 +21,17 @@ class Pokemon extends React.Component {
 }
 
 Pokemon.propTypes = {
-  poke: PropTypes.object,
+  poke: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    image: PropTypes.string,
+    moreInfo: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number,
+      measurementUnit: PropTypes.string,
+    })
+  }).isRequired,
 }
 
 export default Pokemon;
