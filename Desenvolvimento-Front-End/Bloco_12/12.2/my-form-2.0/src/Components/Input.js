0afde9cfebcf3def190bd70ab1da1299a836ffb9
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-25 19:21:23 -0300

    finalizando componente input e func changeHandle

--- Desenvolvimento-Front-End/Bloco_12/12.2/my-form-2.0/src/Components/Input.js
+++ Desenvolvimento-Front-End/Bloco_12/12.2/my-form-2.0/src/Components/Input.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 class Input extends React.Component {
   render() {
+    const { inputLabel, inputType, inputName, inputOnChange, inputValue } = this.props;
     return(
-      
+      <label htmlFor={ inputName }>
+        { inputLabel }
+        <input
+        type={ inputType }
+        name={ inputName }
+        onChange= { inputOnChange }
+        value= { inputValue }
+        />
+      </label>
     );
   }
 }
+
+export default Input;
--- Desenvolvimento-Front-End/Bloco_12/12.2/my-form-2.0/src/Components/PersonalData.js
+++ Desenvolvimento-Front-End/Bloco_12/12.2/my-form-2.0/src/Components/PersonalData.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Input from './Input';
 
 class PersonalData extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.changeHandle = this.changeHandle.bind(this);
+
+    this.state = {
+      nome: '',
+
+    }
+  }
+
+  changeHandle(event) {
+    const { target } = event;
+    const { name } = target;
+    const value = target.checked === true ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    })
+  }
+
   render() {
     return (
       <fieldset>
-
+        <Input 
+        inputLabel="Nome:"
+        inputType="text"
+        inputName="nome"
+        inputOnChange={ this.changeHandle } />
       </fieldset>
     );
   }
