552bc7eef6947645e8f8a3ce88d24118d34676f4
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-25 19:10:08 -0300

    criando componentes datafieldset e input

---
+++ Desenvolvimento-Front-End/Bloco_12/12.2/my-form-2.0/src/Components/PersonalData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PersonalData extends React.Component {
+  render() {
+    return (
+      <fieldset>
+
+      </fieldset>
+    );
+  }
+}
+
+export default PersonalData;
