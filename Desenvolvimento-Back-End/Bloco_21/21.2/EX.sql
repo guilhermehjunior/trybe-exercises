c0a62707a8d90d8480865e13b0bd54a5daafc403
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-17 18:47:59 -0300

    adicionadno ex3 e 4

--- Desenvolvimento-Back-End/Bloco_21/21.2/EX.sql
+++ Desenvolvimento-Back-End/Bloco_21/21.2/EX.sql
@@ -24,4 +24,15 @@ ON m.id = b.movie_id
 WHERE b.domestic_sales < b.international_sales;
 
 -- 3
+SELECT m.title, b.rating
+FROM Movies AS m
+INNER JOIN BoxOffice AS b
+ON m.id = b.movie_id
+ORDER BY b.rating DESC;
 
+-- 4
+SELECT * 
+FROM Theater AS t
+LEFT JOIN Movies AS m
+ON t.id = m.theater_id
+ORDER BY t.`name`;
