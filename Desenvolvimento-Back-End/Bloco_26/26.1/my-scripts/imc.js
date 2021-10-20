1d9c906a1d6689ec78c03c1b77481b7a01c11848
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-20 15:31:39 -0300

    add tabela do imc ao pacote

--- Desenvolvimento-Back-End/Bloco_26/26.1/my-scripts/imc.js
+++ Desenvolvimento-Back-End/Bloco_26/26.1/my-scripts/imc.js
@@ -3,6 +3,13 @@ const readline = require('readline-sync');
 const peso = readline.questionFloat('Qual seu peso? ');
 const altura = readline.questionFloat('Qual sua altura?' );
 
-const imc = altura / (peso * peso);
+const imc = peso / (altura * altura);
 
 console.log(`IMC de ${imc}`);
+
+if (imc < 18.5) console.log('Abaixo do peso');
+else if (imc < 24.9) console.log('Peso Normal');
+else if (imc < 29.9) console.log('Acima do Peso');
+else if (imc < 34.9) console.log('Obesidade grau I');
+else if (imc < 39.9) console.log('Obesidade grau II');
+else console.log('Obesidade Grau III e IV');
