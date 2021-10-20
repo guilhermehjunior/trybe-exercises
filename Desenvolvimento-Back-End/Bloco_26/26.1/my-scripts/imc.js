d3f4b630ee50ec170f9f100d95396c5ca10445d8
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-20 15:25:36 -0300

    utilizando readline para receber peso e altura da pessoa

--- Desenvolvimento-Back-End/Bloco_26/26.1/my-scripts/imc.js
+++ Desenvolvimento-Back-End/Bloco_26/26.1/my-scripts/imc.js
@@ -1,3 +1,8 @@
-const imc = 80 / (1.7 * 1.7);
+const readline = require('readline-sync');
+
+const peso = readline.questionFloat('Qual seu peso? ');
+const altura = readline.questionFloat('Qual sua altura?' );
+
+const imc = altura / (peso * peso);
 
 console.log(`IMC de ${imc}`);
