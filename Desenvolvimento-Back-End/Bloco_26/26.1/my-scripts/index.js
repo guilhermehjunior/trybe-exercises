34d5841ee2f155ae343ebcafea3a306b4fbaa7fa
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-20 16:22:06 -0300

    add exercicio bonus do fatorial

--- Desenvolvimento-Back-End/Bloco_26/26.1/my-scripts/index.js
+++ Desenvolvimento-Back-End/Bloco_26/26.1/my-scripts/index.js
@@ -1,11 +1,12 @@
 const imc = require('./imc');
 const velocidade = require('./velocidade');
 const sorteio = require('./sorteio');
+const fatorial = require('./fatorial');
 const readline = require('readline-sync');
 
 function escolhaScript() {
-  const scripts = ['imc', 'velocidade', 'sorteio'];
-  const funcs = [imc, velocidade, sorteio];
+  const scripts = ['imc', 'velocidade', 'sorteio', 'fatorial'];
+  const funcs = [imc, velocidade, sorteio, fatorial];
   const index = readline.keyInSelect(scripts, 'Escolha o script para rodar. ');
  funcs[index]();
 }
