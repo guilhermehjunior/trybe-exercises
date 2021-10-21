dc271b5f6dcdbafa39bf357e87896c94f03303dc
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-21 18:17:35 -0300

    adicionnando try catch no bonus 1 e arquivo do bonus2

--- Desenvolvimento-Back-End/Bloco_26/26.2/bonus1.js
+++ Desenvolvimento-Back-End/Bloco_26/26.2/bonus1.js
@@ -2,9 +2,13 @@ const readline = require('readline-sync');
 const fs = require('fs').promises;
 
 const userChoosesFile = async () => {
-  const file = readline.question('Qual o arquivo deseja ler?\n');
-  const fileText = await fs.readFile(`./${file}`, 'utf-8');
-  console.log(`O texto do arquivo eh: ${fileText}`);
+  try {
+    const file = readline.question('Qual o arquivo deseja ler?\n');
+    const fileText = await fs.readFile(`./${file}`, 'utf-8');
+    console.log(`O texto do arquivo eh: ${fileText}`);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 userChoosesFile();
