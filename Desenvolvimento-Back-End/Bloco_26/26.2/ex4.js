5accd77bfec95fb5aaceb0dbd5626a31bd6288a1
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-21 15:34:37 -0300

    add ex4.2

--- Desenvolvimento-Back-End/Bloco_26/26.2/ex4.js
+++ Desenvolvimento-Back-End/Bloco_26/26.2/ex4.js
@@ -1,9 +1,31 @@
 const simpsons = require('./simpsons.json');
 
-function simpsonsIdName(characterArray){
+function simpsonsIdName(characterArray) {
   characterArray.forEach(({id, name}) => {
     console.log(`${id} - ${name}`);
   });
 }
 
-simpsonsIdName(simpsons);
+// simpsonsIdName(simpsons);
+
+
+function charNameById(id) {
+  const promise = new Promise((resolve, reject) => {
+    simpsons.forEach((simpson) => {
+      if (simpson.id === id) return resolve(simpson);
+    });
+    reject(new Error('id não encontrado'));
+  });
+  return promise;
+}
+
+// async function char(id) {
+//   try {
+//     const simpson = await charNameById(id);
+//     console.log(`${simpson.id} - ${simpson.name}`)
+//   } catch(e) {
+//     console.log(e.message);
+//   }
+// }
+
+// char("17");
