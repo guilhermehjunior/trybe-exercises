60753bda0c03c6884258665e30f68d38be3c8766
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-21 16:01:32 -0300

    add ex 4.3

--- Desenvolvimento-Back-End/Bloco_26/26.2/ex4.js
+++ Desenvolvimento-Back-End/Bloco_26/26.2/ex4.js
@@ -33,3 +33,21 @@ function charNameById(id) {
 // }
 
 // char("1");
+
+
+async function removeChars() {
+  try {
+    const simpsons = JSON.parse(await fs.readFile('./simpsons.json', 'utf-8'));
+    simpsons.forEach(({id}, index) => {
+      if (id === "6" || id ==="10") {
+        simpsons.splice(index, 1);
+      }
+    });
+    await fs.writeFile('./simpsons.json', JSON.stringify(simpsons));
+    console.log('Arquivo escrito com sucesso');
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+removeChars();
