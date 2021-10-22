c90cfc470c2c1c5a0e0722aadda38f21e008a56b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-22 15:15:15 -0300

    renomeando arquivo, add funcionalidade a funcao de verificar se eh numero e o teste

--- Desenvolvimento-Back-End/Bloco_26/26.3/index.js
+++ Desenvolvimento-Back-End/Bloco_26/26.3/index.js
@@ -1,7 +1,8 @@
 function numberSignal(number) {
+  if (typeof number !== 'number') return 'o valor deve ser um número';
   if (number < 0) return 'Negativo';
   if (number === 0) return 'Neutro';
   return 'Positivo'
 }
 
-module.exports = numberSignal;
+module.exports = {numberSignal};
--- Desenvolvimento-Back-End/Bloco_26/26.3/tests/numberSignal.test.js
+++ Desenvolvimento-Back-End/Bloco_26/26.3/tests/numberSignal.test.js
@@ -1,7 +1,12 @@
 const { expect } = require('chai');
-const numberSignal = require('../index');
+const { numberSignal } = require('../index');
 
 describe('testes funcao para identificar sinal de numeros', () => {
+  it('O argumento recebido nao eh do tipo numero', () => {
+    const result = numberSignal('Oi');
+    expect(result).to.be.equal('o valor deve ser um número');
+  });
+  
   it('O numero eh positivo', () => {
     const result = numberSignal(1);
     expect(result).to.be.equal('Positivo');
