b14d8f06f69acfff70421ec108a81423c6f21fff
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-25 15:41:09 -0300

    add post na rota /hello

--- Desenvolvimento-Back-End/Bloco_26/26.4/index.js
+++ Desenvolvimento-Back-End/Bloco_26/26.4/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // 1
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+// 2
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}!` });
 });
 
 app.listen(3000, () => {
