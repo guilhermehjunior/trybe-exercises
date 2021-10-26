593ef9e126ed50fc86193deeb55abc6d9fcb982f
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-10-26 17:30:42 -0300

    arrumando atividade 2

--- Desenvolvimento-Back-End/Bloco_26/26.5/controllers/bitcoinFetch.js
+++ Desenvolvimento-Back-End/Bloco_26/26.5/controllers/bitcoinFetch.js
@@ -1,8 +1,12 @@
 const URL = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json)';
 
 module.exports = async (_req, res, next) => {
-  const response = await fetch(URL);
-
-  res.status(201).json(response.data);
-  next();
+  try {
+    const response = await fetch(URL);
+  
+    res.status(201).json(response.data);
+    next();
+  } catch (e) {
+    next(e);
+  }
 };
--- Desenvolvimento-Back-End/Bloco_26/26.5/middlewares/tokenValidation.js
+++ Desenvolvimento-Back-End/Bloco_26/26.5/middlewares/tokenValidation.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   const regex = /\w{12}/;
-  if (!regex.test(autorizhation)) return res.status(400).json({ "message": "invalid data" });
+  if (!regex.test(authorization)) return res.status(400).json({ "message": "invalid data" });
   next();
 };
