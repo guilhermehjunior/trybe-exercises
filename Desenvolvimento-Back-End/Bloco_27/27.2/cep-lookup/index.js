79c6ff4555a61b40ae5daaa6a8b3c833b217aa73
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-11-12 15:36:02 -0300

    add endpoin ping

--- Desenvolvimento-Back-End/Bloco_27/27.2/cep-lookup/index.js
+++ Desenvolvimento-Back-End/Bloco_27/27.2/cep-lookup/index.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
 app.listen(PORT, () => {
   console.log(`ON PORT ${PORT}`);
 });
