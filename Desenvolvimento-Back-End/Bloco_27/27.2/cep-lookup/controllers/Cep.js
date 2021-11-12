b6179befce401162dd5f90b9b2ed4df9402e7749
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-11-12 16:18:09 -0300

    criando controller e adicionando ao endpoint /cep/:cep

--- Desenvolvimento-Back-End/Bloco_27/27.2/cep-lookup/controllers/Cep.js
+++ Desenvolvimento-Back-End/Bloco_27/27.2/cep-lookup/controllers/Cep.js
@@ -1,8 +1,11 @@
-const { getCepByCep } = require('../services/Cep');
+const Cep = require('../services/Cep');
 
-const getCepByCep = (req, res, next) => {
+const getCepByCep = async (req, res, next) => {
   try {
-    const cep = await getCepByCep()
+    const { cep } = req.params;
+    const cepEncontrado = await Cep.getCepByCep(cep);
+    if(cepEncontrado.error) return res.status(cepEncontrado.code).json({ error: cepEncontrado.error });
+    res.status(cepEncontrado.code).json(cepEncontrado.cep);
   } catch(err) {
     next(err);
   }
--- Desenvolvimento-Back-End/Bloco_27/27.2/cep-lookup/index.js
+++ Desenvolvimento-Back-End/Bloco_27/27.2/cep-lookup/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Cep = require('./controllers/Cep');
 
 const app = express();
 const PORT = 3000;
@@ -10,9 +11,7 @@ app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
-app.get('/cep/:cep', (req, res) => {
-  
-});
+app.get('/cep/:cep', Cep.getCepByCep);
 
 app.listen(PORT, () => {
   console.log(`ON PORT ${PORT}`);
