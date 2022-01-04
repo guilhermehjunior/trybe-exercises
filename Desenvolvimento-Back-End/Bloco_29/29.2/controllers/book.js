7c64a8aca1a664f1192f10761d566352c72df04a
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-01-04 18:59:55 -0300

    criando os endpoints

--- Desenvolvimento-Back-End/Bloco_29/29.2/controllers/book.js
+++ Desenvolvimento-Back-End/Bloco_29/29.2/controllers/book.js
@@ -13,4 +13,48 @@ bookRouter.post('/', async (req, res) => {
   }
 });
 
+bookRouter.get('/', async (_req,res) => {
+  try {
+    const books = await Book.findAll();
+    return res.status(200).json(books);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).end();
+  }
+});
+
+bookRouter.get('/:id', async (req,res) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findByPk(id);
+    return res.status(200).json(book);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).end();
+  }
+});
+
+bookRouter.delete('/:id', async (req,res) => {
+  const { id } = req.params;
+  try {
+    await Book.destroy({ where: { id } });
+    return res.status(200).end();
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).end();
+  }
+});
+
+bookRouter.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, author, pageQuantity } = req.body;
+  try {
+    const newBook = await Book.update({ title, author, pageQuantity }, { where: { id } });
+    return res.status(201).json(newBook);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).end();
+  }
+});
+
 module.exports = bookRouter;
