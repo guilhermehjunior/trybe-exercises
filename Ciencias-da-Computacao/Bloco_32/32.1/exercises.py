6187e2539e54496fbfd097575633fd6cd4b0585b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-02-08 16:28:55 -0300

    add ex 3

--- Ciencias-da-Computacao/Bloco_32/32.1/exercises.py
+++ Ciencias-da-Computacao/Bloco_32/32.1/exercises.py
@@ -15,4 +15,18 @@ def mediaAritimetica(list):
         total += number
     return total / len(list)
 
+
 print(mediaAritimetica([1, 2, 3]))
+
+#Exercício 3: Faça um programa que, dado um valor n qualquer, tal que n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+
+def squarePrinter(sides):
+    index = 1
+    while index < sides + 1:
+        print(sides * '*')
+        index += 1
+
+
+squarePrinter(4)
+
