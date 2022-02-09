5b72b37e9d62f56174617816f9a82088b59ce904
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-02-09 17:53:30 -0300

    add ex 2

--- Ciencias-da-Computacao/Bloco_32/32.2/ex.py
+++ Ciencias-da-Computacao/Bloco_32/32.2/ex.py
@@ -12,3 +12,41 @@ def retiraLetrasDoNomePorLinha(word):
 if __name__ == "__main__":
     name = input("Digite um nome: ")
     retiraLetrasDoNomePorLinha(name)
+
+# Exercício 2: Jogo da palavra embaralhada. Desenvolva um jogo em que a pessoa usuária tenha que adivinhar uma palavra que será mostrada com as letras embaralhadas. O programa terá uma lista de palavras e escolherá uma aleatoriamente. O jogador terá três tentativas para adivinhar a palavra. Ao final a palavra deve ser mostrada na tela, informando se a pessoa ganhou ou perdeu o jogo.
+
+
+import random
+words = ["abutre", "cachorro", "asterix", "sistema"]
+
+
+def random_word(palavras):
+    return random.choice(palavras)
+
+
+def scramble_word(palavra):
+    return "".join(random.sample(palavra, len(palavra)))
+
+
+def guesses():
+    chutes = []
+    index = 0
+    while index < 3:
+        chutes.append(input('Qual a palavra embaralhada?'))
+        index += 1
+    return chutes
+
+
+def main(palavras):
+    palavra_escolhida = random_word(palavras)
+    palavra_embaralhada = scramble_word(palavra_escolhida)
+    print(palavra_embaralhada)
+    chutes = guesses()
+    if palavra_escolhida in chutes:
+        print(f'Ganhou! A palavra era {palavra_escolhida}')
+    else:
+        print(f'Errou! A palavra era {palavra_escolhida}')
+
+
+if __name__ == "__main__":
+    main(words)
--- Ciencias-da-Computacao/Bloco_32/32.2/tempCodeRunnerFile.py
+++ Ciencias-da-Computacao/Bloco_32/32.2/tempCodeRunnerFile.py
@@ -1,12 +1,34 @@
-def retiraLetrasDoNomePorLinha(word):
+import random
+words = ["abutre", "cachorro", "asterix", "sistema"]
+
+
+def random_word(palavras):
+    return random.choice(palavras)
+
+
+def scramble_word(palavra):
+    return "".join(random.sample(palavra, len(palavra)))
+
+
+def guesses():
+    chutes = []
     index = 0
-    while index < len(word):
-        for letter in range(len(word) - index):
-            print(word[letter], end="")
-        print()
+    while index < 3:
+        chutes.append(input('Qual a palavra embaralhada?'))
         index += 1
+    return chutes
+
+
+def main(palavras):
+    palavra_escolhida = random_word(palavras)
+    palavra_embaralhada = scramble_word(palavra_escolhida)
+    print(palavra_embaralhada)
+    chutes = guesses()
+    for chute in chutes:
+        if chute == palavra_escolhida:
+            return print(f'Ganhou! A palavra era {palavra_escolhida}')
+    print(f'Errou! A palavra era {palavra_escolhida}')
 
 
 if __name__ == "__main__":
-    name = input("Digite um nome: ")
-    retiraLetrasDoNomePorLinha(name)
+    main(words)
