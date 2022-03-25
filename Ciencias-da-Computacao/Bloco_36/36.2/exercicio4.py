e44d406e80f5f2d7b7f5a427b8aec95c50ac02ba
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-03-25 16:23:34 -0300

    ex4

---
+++ Ciencias-da-Computacao/Bloco_36/36.2/exercicio4.py
@@ -0,0 +1,9 @@
+def pessoas_na_biblioteca(entradas, saidas, hora_buscada):
+    pessoas = 0
+    for index, hora in enumerate(entradas):
+        if hora < hora_buscada < saidas[index]:
+            pessoas += 1
+    return pessoas
+
+
+print(pessoas_na_biblioteca([1, 2, 2], [4, 2, 7], 3))
