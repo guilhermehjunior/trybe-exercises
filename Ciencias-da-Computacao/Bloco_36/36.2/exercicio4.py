57718fe512591b6057f6534ad3f6f3788a927668
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2022-03-25 16:24:45 -0300

    add gabarito do 4 pq eh interessante

--- Ciencias-da-Computacao/Bloco_36/36.2/exercicio4.py
+++ Ciencias-da-Computacao/Bloco_36/36.2/exercicio4.py
@@ -7,3 +7,10 @@ def pessoas_na_biblioteca(entradas, saidas, hora_buscada):
 
 
 print(pessoas_na_biblioteca([1, 2, 2], [4, 2, 7], 3))
+
+
+# def students_in_instant(arrivals, departures, time_instant):
+#     return sum(
+#         arrival < time_instant < departure
+#         for arrival, departure in zip(arrivals, departures)
+#     )
